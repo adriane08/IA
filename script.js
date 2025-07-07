4574aca3903a7bb2b53cedeab7e0445a6f68e32d
adriane08 <139055906+adriane08@users.noreply.github.com>
2025-07-07 08:26:53 -0300

    ...

--- script.js
+++ script.js
@@ -5,87 +5,6 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-{
-enunciado: "Você já deve ter percebido que o nosso planeta está mudando. Qual o seu primeiro pensamento?",
-alternativas: [
-{
-texto: "Isso é assustador!",
-afirmacao: "Não sei o que vai acontecer com o planeta."
-},
-{
-texto: "Isso é normal.",
-afirmacao: "Como ajudar o planeta."
-}
-]
-},
-{
-enunciado: "Com as chamadas mudanças climáticas, temos um novo desafio: diminuir os impactos no meio ambiente. Qual atitude você toma?",
-alternativas: [
-{
-texto: "Faz a sua parte, com pequenas atitudes diárias",
-afirmacao: "Sabe que vai fazer a diferença."
-},
-{
-texto: "Não liga para esse assunto.",
-afirmacao: "Sabe que não vai fazer diferença."
-}
-]
-},
-{
-        enunciado: "O planeta não é mais o mesmo e, o que você fez para mudar isso......",
-        enunciado: "Em 2049 o planeta não é mais o mesmo e, o que você fez para mudar isso......"
-
-},
-
-
-];
-
-
-let atual = 0;
-let perguntaAtual;
-let historiaFinal = "";
-
-function mostraPergunta() {
-if (atual >= perguntas.length) {
-mostraResultado();
-return;
-}
-perguntaAtual = perguntas[atual];
-caixaPerguntas.textContent = perguntaAtual.enunciado;
-caixaAlternativas.textContent = "";
-mostraAlternativas();
-}
-
-function mostraAlternativas(){
-for(const alternativa of perguntaAtual.alternativas) {
-const botaoAlternativas = document.createElement("button");
-botaoAlternativas.textContent = alternativa.texto;
-botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-caixaAlternativas.appendChild(botaoAlternativas);
-}
-}
-
-function respostaSelecionada(opcaoSelecionada) {
-const afirmacoes = opcaoSelecionada.afirmacao;
-historiaFinal += afirmacoes + " ";
-atual++;
-mostraPergunta();
-}
-
-function mostraResultado() {
-caixaPerguntas.textContent = "Em 2049...";
-textoResultado.textContent = historiaFinal;
-caixaAlternativas.textContent = "";
-}
-
-
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
-
-const perguntas = [
     {
         enunciado: "Você já deve ter percebido que o nosso planeta está mudando. Qual o seu primeiro pensamento?",
         alternativas: [
@@ -104,11 +23,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Faz a sua parte, com pequenas atitudes diárias.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                afirmacao: "Sabe que vai fazer a diferença."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Não liga para esse assunto.",
+                afirmacao: "Sabe que não vai fazer diferença."
             }
         ]
     },
@@ -146,7 +65,7 @@ const perguntas = [
                 afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
+                texto: "O planeta não é mais o mesmo e, o que você fez para mudar isso......",
                 afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
             }
         ]
