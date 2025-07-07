fa0a863ec81bdc4e60f4985c4e67f0bdb06783b8
adriane08 <139055906+adriane08@users.noreply.github.com>
2025-07-07 08:12:09 -0300

    ...

--- script.js
+++ script.js
@@ -1,41 +1,159 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+const textoResultado = document.querySelector(".texto-resultado");
+
+
+
 const perguntas = [
-    {
-        enunciado: "Você já deve ter percebido que o nosso planeta está mudando. Qual o seu primeiro pensamento?",
-        alternativas: [
-            {
-                texto: "Isso é assustador!",
-                afirmacao: "Você sente apreensão sobre o futuro do planeta."
-            },
-            {
-                texto: "Isso é normal.",
-                afirmacao: "Você acredita que o planeta passa por ciclos naturais de mudança."
-            }
-        ]
-    },
-    {
-        enunciado: "Com as chamadas mudanças climáticas, temos um novo desafio: diminuir os impactos no meio ambiente. Qual atitude você toma?",
-        alternativas: [
-            {
-                texto: "Faz a sua parte, com pequenas atitudes diárias",
-                afirmacao: "Você se esforça para contribuir individualmente."
-            },
-            {
-                texto: "Não liga para esse assunto.",
-                afirmacao: "Você sente que suas ações não teriam impacto significativo."
-            }
-        ]
-    },
-    {
-        enunciado: "Em 2049, o planeta não é mais o mesmo. Pensando nas suas ações, o que você fez para mudar isso?",
-        alternativas: [
-            {
-                texto: "Você se tornou um defensor ativo e participou de iniciativas ambientais.",
-                afirmacao: "Suas ações foram fundamentais para conscientizar e impulsionar a mudança."
-            },
-            {
-                texto: "Você priorizou outras áreas da sua vida e observou as mudanças acontecerem.",
-                afirmacao: "Você testemunhou as transformações, mas suas prioridades estavam em outro lugar."
-            }
-        ]
-    }
+
+{
+
+enunciado: "Você já deve ter percebido que o nosso planeta está mudando. Qual o seu primeiro pensamento?",
+
+alternativas: [
+
+{
+
+texto: "Isso é assustador!",
+
+afirmacao: "Não sei o que vai acontecer com o planeta."
+
+},
+
+{
+
+texto: "Isso é normal.",
+
+afirmacao: "Como ajudar o planeta."
+
+}
+
+]
+
+},
+
+{
+
+enunciado: "Com as chamadas mudanças climáticas, temos um novo desafio: diminuir os impactos no meio ambiente. Qual atitude você toma?",
+
+alternativas: [
+
+{
+
+texto: "Faz a sua parte, com pequenas atitudes diárias",
+
+afirmacao: "Sabe que vai fazer a diferença."
+
+},
+
+{
+
+texto: "Não liga para esse assunto.",
+
+afirmacao: "Sabe que não vai fazer diferença."
+
+}
+]
+},
+
+
+ [ {
+
+enunciado: "Em 2049 o planeta não é mais o mesmo e, o que você fez para mudar isso......"
+
+
+}
+
+
+
+
+] 
+
+
 ];
+
+
+
+
+
+let atual = 0;
+
+let perguntaAtual;
+
+let historiaFinal = "";
+
+
+
+function mostraPergunta() {
+
+if (atual >= perguntas.length) {
+
+mostraResultado();
+
+return;
+
+}
+
+perguntaAtual = perguntas[atual];
+
+caixaPerguntas.textContent = perguntaAtual.enunciado;
+
+caixaAlternativas.textContent = "";
+
+mostraAlternativas();
+
+}
+
+
+
+function mostraAlternativas(){
+
+for(const alternativa of perguntaAtual.alternativas) {
+
+const botaoAlternativas = document.createElement("button");
+
+botaoAlternativas.textContent = alternativa.texto;
+
+botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+
+caixaAlternativas.appendChild(botaoAlternativas);
+
+}
+
+}
+
+
+
+function respostaSelecionada(opcaoSelecionada) {
+
+const afirmacoes = opcaoSelecionada.afirmacao;
+
+historiaFinal += afirmacoes + " ";
+
+atual++;
+
+mostraPergunta();
+
+}
+
+
+
+function mostraResultado() {
+
+caixaPerguntas.textContent = "Em 2049...";
+
+textoResultado.textContent = historiaFinal;
+
+caixaAlternativas.textContent = "";
+
+}
+
+
+
+mostraPergunta();
